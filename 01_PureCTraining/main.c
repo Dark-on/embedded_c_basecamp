837c354f3ba9e67bdff2ed54767aacdf3dea38fe
Dark-on <Sipulka.Dr@gmail.com>
2022-12-05 18:10:45 +0200

    Completed first task

---
+++ 01_PureCTraining/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void){
+	printf("Group-2\n");
+	printf("Svitlana Drozd\n");
+	printf("Email: drozd.svit@gmail.com\n");
+	printf("Repository link: https://github.com/Dark-on/embedded_c_basecamp\n");
+	printf("Shared folder link: https://drive.google.com/drive/folders/1F3OUn4NcgWEBuXgmKPKLYvBAvIS09S2j?usp=sharing\n");
+
+	printf("\nPress enter key to exit the application");
+	getchar();
+	return 1;
+}
